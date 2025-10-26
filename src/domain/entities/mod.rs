184e267bade33d46596b99718e948af976a9770a
Celine <enberkaych@gmail.com>
2025-10-26 21:49:20 +0700

    feat: Add permission entity

--- src/domain/entities/mod.rs
+++ src/domain/entities/mod.rs
@@ -1,5 +1,6 @@
 pub mod book;
 pub mod branch;
 pub mod category;
+pub mod permission;
 pub mod role;
 pub mod user;
---
+++ src/domain/entities/permission.rs
@@ -0,0 +1,9 @@
+use chrono::{DateTime, Utc};
+
+#[derive(Debug, Clone)]
+pub struct PermissionEntity {
+    pub id: i32,
+    pub name: String,
+    pub description: Option<String>,
+    pub created_at: DateTime<Utc>,
+}
